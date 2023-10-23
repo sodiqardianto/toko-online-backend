8f0c3a5e954edd46f81c2b1a63870a7a762a4029
sodiqardianto <sodiqardianto@gmail.com>
2023-10-23 13:41:34 +0700

    first commit

---
+++ database/migrations/2023_10_23_044134_create_transaction_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transactions_id')->constrained('transactions');
+            $table->foreignId('products_id')->constrained('products');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
